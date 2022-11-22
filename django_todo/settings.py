aa35f1524b6f1db21dbe236d4558f1ffb2d18154
Samuel Näsström <nasstromsamuel@icloud.com>
2022-11-22 12:07:47 +0000

    Set up environment variables and automatic deployments

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRETKEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['git.heroku.com/fssammy-django-todo-app.git']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
